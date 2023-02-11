988f795586fbbc3a27e181c4192e14950d979935
Sudarshan Hosalli@DESKTOP-F068LQM <godhustler90956@gmail.com>
2023-02-11 23:09:57 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Welcome to the Brand Name Generator.")
+city = input("Which City did you grow up in?\n ")
+pet = input("What is your pet name? \n")
+add = city + pet
+
+print("your brand name is " + add )
